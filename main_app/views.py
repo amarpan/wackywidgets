d453d0a43626a0bfc57059cb00709a3f4776e0c3
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-29 13:27:27 -0700

    add basic starter code

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html')
+
+# Create your views here.
